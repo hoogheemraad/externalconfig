34998de7a6f7b6726582f2988a2b0a70dc075a9d
Patrick Hutchinson <patrick.hutchinson@sparcedge.com>
2012-07-06 10:57:33 -0400

    added ability to load Properties from URL

--- app/plugin/PropertiesFileConfigurationPlugin.java
+++ app/plugin/PropertiesFileConfigurationPlugin.java
@@ -1,6 +1,11 @@
 package plugin;
 
+import java.io.*;
 import java.io.IOException;
+import java.lang.Exception;
+import java.lang.String;
+import java.net.*;
+import java.net.URL;
 import java.util.Map.Entry;
 import java.util.Properties;
 
@@ -17,12 +22,50 @@ public class PropertiesFileConfigurationPlugin extends PlayPlugin {
 		
 		for (String propertiesFilename : propertiesFilenames) {
 			Logger.info("Loading configuration from " + propertiesFilename);
-			Properties properties = IO.readUtf8Properties(this.getClass().getResourceAsStream(propertiesFilename));
-			
-			for (Entry<Object, Object> entry : properties.entrySet()) {
+
+            Properties properties = null;
+            try {
+                properties = loadPropertiesFromAppropriateSource(propertiesFilename);
+            } catch (IOException e) {
+                Logger.error(e, "Unable to load properties from file: " + propertiesFilename);
+            }
+
+
+            for (Entry<Object, Object> entry : properties.entrySet()) {
 				Play.configuration.setProperty((String) entry.getKey(),(String) entry.getValue());
 			}
 		}
 	}
+
+    /**
+     * Determines if the given String represents a potentially valid URL.
+     * @param value String value to check and see if it represents a URL.
+     * @return whether the given String represents a potentially valid URL.
+     */
+    private static boolean isUrl(String value){
+        return value != null && (value.startsWith("http://") || value.startsWith("https://"));
+    }
+
+    /**
+     * Loads a new Properties object from the given String argument which may point to a file on the Classpath or,
+     * alternatively a URL.
+     * @param propertiesFilename String referencing the properties file to be loaded.
+     * @return Properties oject populated by the given propertiesFilename.
+     */
+    private Properties loadPropertiesFromAppropriateSource(String propertiesFilename) throws IOException{
+        Properties properties = null;
+        if(isUrl(propertiesFilename)){
+            properties = new Properties();
+
+            URL url = new URL(propertiesFilename);
+            url.openConnection();
+
+            properties.load(url.openStream());
+        } else {
+            properties = IO.readUtf8Properties(this.getClass().getResourceAsStream(propertiesFilename));
+        }
+
+        return properties;
+    }
 
 }
